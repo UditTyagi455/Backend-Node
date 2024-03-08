a18260fb17da3d5a7f0f2462ba970788ab8c5649
Udit tyagi <udittyagi455@gmail.com>
2024-03-08 23:11:44 +0530

    Database connection successfully

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config({path: "./env"})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// ;(async () =>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error",(error) => {
+//         console.log("Error ::",error);
+//         throw error;
+//        })
+
+//        app.listen(process.env.PORT, () => {
+//         console.log(`App is listening at ${process.evn.PORT}`);
+//        });
+
+//     } catch (error) {
+//         console.log("error ::",error);
+//     }
+// })()
