23259777249bcac40a7a407dea246767b1825c67
udittyagi455 <udittyagi455@gmail.com>
2024-03-09 21:32:31 +0530

    first post (register) api created

--- src/app.js
+++ src/app.js
@@ -12,6 +12,16 @@ import cors from "cors";
  app.use(express.json({limit: "16kb"}));
  app.use(express.urlencoded({extended: true,limit: "16kb"}));
  app.use(express.static("public"));
- app.use(cookieParser())
+ app.use(cookieParser());
+
+
+ //import router
+ import userRouter from "./routes/user.routes.js";
+
+
+ //routes declare
+ app.use("/api/v1/users",userRouter);
+
+ //http://localhost:8000/api/v1/users/register
 
  export { app }
