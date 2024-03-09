b7615a5941875af4d5903d5ecaeca2ed22f3048e
Udit tyagi <udittyagi455@gmail.com>
2024-03-09 12:12:54 +0530

    util code setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 ,() => {
+        console.log(`App is listing at port : ${process.env.PORT}`);
+    })
+    app.on("error",(err) => {
+        console.log(`Error in connecting the App :: ${err}`);
+    })
+})
+.catch((err) => console.log(`Error in connect to database !!!`,err))
 
 
 
