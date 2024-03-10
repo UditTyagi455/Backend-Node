31ff4aa87fb30b4e956f58ce6e38bc86286e3c3f
udittyagi455 <udittyagi455@gmail.com>
2024-03-10 13:01:32 +0530

    password encryped done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import Jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
@@ -50,7 +50,44 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password =await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken =function () {
+  return Jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken =function () {
+  return Jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 
 export const UserModel = mongoose.model("User", userSchema);
+
 
--- src/utils/cloudinary.service.js
+++ src/utils/cloudinary.service.js
@@ -16,7 +16,8 @@ try {
         resource_type: "auto"
     })
     //file has been uploaded successfully
-    console.log("file is uploaded on cloudinary",response.url);
+    // console.log("file is uploaded on cloudinary",response.url);
+    fs.unlinkSync(localFilePath);
     return response;
 } catch (error) {
     fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload opration got failed
